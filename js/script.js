32bf614e30f9fa4554c409cef424770eca7f8137
mtallerico1 <mtallerico1@gmail.com>
2017-12-02 07:52:00 -0500

    Making changes to have the background color randomly change.

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 // declaring an array of objects to contain quotes and pertinent information
 var quotes = [
-    { quote: "For there is nothing lost, that may be found, if sought." , source:"Edmund Spenser", citation:"The Faerie Queen" },
-    { quote: "Don't cry because it's over, smile because it happened" , source: "Dr. Suess" },
-    { quote: "So many books, so little time." , source: "Frank Zappa" },
-    { quote: "If you tell the truth, you don't have to remember anything.", source: "Mark Twain" },
-    { quote: "Thou hast made us for thyself, O Lord, and our heart is restless until it finds its rest in thee.", source: "Augustine of Hippp", citation: "Confessions" },
-    { quote: "Love your neighbor as yourself.", source: "Jesus", citation: "The Book of Matthew" },
-    { quote: "Failures, repeated failues, are finger post on the road to achievement. One fails forward toward success.", source: "C.S. Lewis" },
-    { quote: "It is never too late to be what you might have been.", source: "George Elliot" },
-    { quote: "There is no greater agony than bearing an untold story inside you.", source: "Maya Angelou", citation: "I Know Why the Caged Bird Sings", year: 1970 },
-    { quote: "Do what you can, with what you have, where you are.", source: "Theodore Roosevelt" }
+    { quote: "For there is nothing lost, that may be found, if sought." , source:"Edmund Spenser", citation:"The Faerie Queen", tag: "inspiration" },
+    { quote: "Don't cry because it's over, smile because it happened" , source: "Dr. Suess", tag: "truth" },
+    { quote: "So many books, so little time." , source: "Frank Zappa", tag: "random" },
+    { quote: "If you tell the truth, you don't have to remember anything.", source: "Mark Twain", tag: "truth" },
+    { quote: "Thou hast made us for thyself, O Lord, and our heart is restless until it finds its rest in thee.", source: "Augustine of Hippp", citation: "Confessions", tag: "religious" },
+    { quote: "Love your neighbor as yourself.", source: "Jesus", citation: "The Book of Matthew", tag: "religious" },
+    { quote: "Failures, repeated failues, are finger post on the road to achievement. One fails forward toward success.", source: "C.S. Lewis", tag: "motivation" },
+    { quote: "It is never too late to be what you might have been.", source: "George Elliot", tag: "inspiration" },
+    { quote: "There is no greater agony than bearing an untold story inside you.", source: "Maya Angelou", citation: "I Know Why the Caged Bird Sings", year: 1970, tag: "uplifting" },
+    { quote: "Do what you can, with what you have, where you are.", source: "Theodore Roosevelt", tag: "motivation" }
 ];
 
 var color = [ '#020202', '#0D2818', '#04471C', '#058C42', '#16DB65' ];
@@ -32,16 +32,17 @@ function getRandomNumber () {
 }
 
 function randomColor () {
-    randomColor = Math.floor( Math.random() * color.length);
-    return color[randomColor];
+    var randomColorHex = Math.floor( Math.random() * color.length);
+    randomColor = color[randomColorHex];
 }
 
-
+randomColor();
 
 
 //this function prints the random quote to the dive on the page
 function printQuote () {
     randomQuote = getRandomNumber();
+    document.getElementById(main).style.backgroundColor = randomColor;
     output = '<p class="quote">' + randomQuote.quote + '</p>';
     // this if else statement looks to see weather the quote has citation and year in the object and then adds the neccesary html 
     if (randomQuote.citation && randomQuote.year) {
@@ -54,6 +55,5 @@ function printQuote () {
     // this targets the correct div in the DOM and prints the string in output
     var htmlDiv = document.getElementById('quote-box'); 
     htmlDiv.innerHTML= output;
-    document.getElementById(body).style.backgroundColor = randomColor();
 }
 
